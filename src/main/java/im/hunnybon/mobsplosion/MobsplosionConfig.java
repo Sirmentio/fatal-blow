e0856aafabad0c9c77b7b071bcd1610dfa45276c
Madeline <42724159+Sirmentio@users.noreply.github.com>
2021-08-16 16:34:28 -0400

    Remove unnecessary imports

--- src/main/java/im/hunnybon/mobsplosion/MobsplosionConfig.java
+++ src/main/java/im/hunnybon/mobsplosion/MobsplosionConfig.java
@@ -1,15 +1,5 @@
 package im.hunnybon.mobsplosion;
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
-
 public class MobsplosionConfig {
     public boolean destroyBlocks;
     public boolean createsFire;
